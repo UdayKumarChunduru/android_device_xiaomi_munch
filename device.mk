fc9808dc7ec5ddd4f4c82d543fc1a073d1af1234
Jackeagle <jackeagle102@gmail.com>
2020-06-08 19:22:04 +0200

    munch: Build libvndfwk_detect_jni.qti.vendor
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I45a11169c0382db0305ac0639903df03385ec38b

--- device.mk
+++ device.mk
@@ -336,6 +336,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# QTI fwk-detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
+
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackages
