656b1ef19f05c038572ca416a92c8a992e67ed0c
Alexander Koskovich <akoskovich@pm.me>
2022-01-22 00:46:40 -0700

    munch: Update telephony package list to LA.QSSI.12.0.r1-05600-qssi.0
    Change-Id: Id6fdabd9d5e0df6d23a982e0e39e06a8acff67ef

--- device.mk
+++ device.mk
@@ -443,14 +443,22 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
+    qti-telephony-hidl-wrapper-prd \
     qti_telephony_hidl_wrapper.xml \
+    qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
+    qti-telephony-utils-prd \
     qti_telephony_utils.xml \
     telephony-ext \
-    xiaomi-telephony-stub
+    xiaomi-telephony-stub \
+    qti_telephony_utils_prd.xml
 
 PRODUCT_BOOT_JARS += \
     telephony-ext \
