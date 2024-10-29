edb2ee49d652857dcc580adab98d86083b66945a
onettboots <blackcocopet@gmail.com>
2024-10-29 16:40:53 +0000

    munch: kernel: Update flags for clang 20+
    clang: error: version 'kernel' in target triple 'arm-unknown-linux-androidkernel' is invalid

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,10 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/munch
 TARGET_KERNEL_CONFIG := vendor/munch_defconfig
 
+# Kernel Clang Flags
+KERNEL_CC := CC=clang
+override KERNEL_TOOLCHAIN_PREFIX_arm := arm-linux-android-
+
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
