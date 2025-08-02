0ee1d10ddd350595eb613909c3a5eb46c7fffd43
UdayKumarChunduru <udaykumarchunduru54@gmail.com>
2025-08-02 22:20:26 +0530

    Update -> munch: Remove unwanted packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -3,8 +3,22 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Chrome-Stub ViaBrowser Chrome64 YouTube Updater
+LOCAL_OVERRIDES_PACKAGES := \
+    Calculator \
+    Calculator2 \
+    CalculatorGoogle \
+    Chrome64 \
+    Chrome-Stub \
+    Drive \
+    FilesGoogle \
+    Glimpse \
+    Photos \
+    Recorder \
+    Updater \
+    ViaBrowser \
+    YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
+
