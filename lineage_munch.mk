0ac9aadce1c4b6d82e783f2296231a3fc875c5fc
UdayKumarChunduru <udaykumarchunduru54@gmail.com>
2025-08-21 21:13:37 +0530

    munch: @Lunaris-AOSP

---
+++ lineage_munch.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common lunaris stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from munch device
+$(call inherit-product, device/xiaomi/munch/device.mk)
+
+PRODUCT_NAME := lineage_munch
+PRODUCT_DEVICE := munch
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Poco
+PRODUCT_MODEL := 22021211RG
+
+WITH_GMS := false
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="munch-user 13 RKQ1.211001.001 V816.0.9.0.ULMMIXM release-keys" \
+    BuildFingerprint=POCO/munch_global/munch:13/RKQ1.211001.001/V816.0.9.0.ULMMIXM:user/release-keys
