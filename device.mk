2aeb347c3cb139891b10fafbc014d736d739252a
johnmart19 <johnivan19999@gmail.com>
2025-08-23 08:27:14 +0300

    munch: Move to the common WFD stack

--- device.mk
+++ device.mk
@@ -456,6 +456,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 # Wi-Fi Display
+$(call inherit-product, device/qcom/wfd/device.mk)
+
 PRODUCT_PACKAGES += \
     libwfdaac_vendor
 
